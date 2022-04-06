00542d65e807b6751fdb3605cafd79f0d9cc9c5e
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-06 19:58:33 -0300

    Atribuição de variáveis

---
+++ 5-atribuicaoDeVariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com atribuição de variáveis")
+
+const idade = 29
+const nome = "Cristina"
+const sobrenome = "Pereira"
+
+console.log(nome, sobrenome)
+console.log(nome + " " + sobrenome)
