f2b65a4fe965dd7e371fd0e13ddc95db8f82eb1f
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-06 19:54:01 -0300

    Correção do comentário

--- 4-conversaoDeTipos.js
+++ 4-conversaoDeTipos.js
@@ -4,4 +4,4 @@ console.log("ano ", 2022)
 console.log("2" + "2")
 console.log(parseInt("2") + parseInt("2"))
 console.log("10" / "2")
-console.log("teste" / "2") //not a number NAM
+console.log("teste" / "2") //not a number NaN
