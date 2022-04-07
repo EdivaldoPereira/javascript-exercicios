44cf64e46b5c2bbc18345d2712423839e6601e6c
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-07 11:59:58 -0300

    Inclusão do embarque

--- 7.1-operadoresEncadeados.js
+++ 7.1-operadoresEncadeados.js
@@ -7,6 +7,8 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 15
 const estaAcompanhado = true
+const temPassagemComprada = true
+
 console.log("Destinos possíveis: ")
 console.log(listaDeDestinos)
 
@@ -18,6 +20,13 @@ if (
     listaDeDestinos.splice(1,1)
 }else{
         console.log("Comprador não é maior de idade. Compra não concluída")
+    }
+
+    console.log("Embraque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem")
+}else{
+    console.log("Você não pode embarcar")
     }
 
 console.log(listaDeDestinos)
