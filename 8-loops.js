195357c274b4232800adc15a40c966914aaedaa9
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-07 12:19:50 -0300

    Criação de loops

---
+++ 8-loops.js
@@ -0,0 +1,32 @@
+console.log(`\nTrabalhando com condicionais`)
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+)
+
+const idadeComprador = 15
+const estaAcompanhado = true
+const temPassagemComprada = true
+
+console.log("\nDestinos possíveis: ")
+console.log(listaDeDestinos)
+
+if (
+    idadeComprador >= 18 ||
+    estaAcompanhado == true
+    ) {
+    console.log("Boa viagem!")
+    listaDeDestinos.splice(1,1)
+}else{
+        console.log("Comprador não é maior de idade. Compra não concluída")
+    }
+
+    console.log("Embraque: \n\n")
+if(idadeComprador >= 18 && temPassagemComprada){
+    console.log("Boa viagem")
+}else{
+    console.log("Você não pode embarcar")
+    }
+
+console.log(listaDeDestinos)
