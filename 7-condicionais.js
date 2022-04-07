4fa9948e746624f7faf90ba0d96e6d648671a850
Edivaldo Pereira <edivaldo.pereira@gmail.com>
2022-04-07 10:59:34 -0300

    Inclusão do if

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -5,7 +5,12 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 )
 
+const idadeComprador = 15
 console.log("Destinos possíveis: ")
-
 console.log(listaDeDestinos)
+
+if (idadeComprador >= 18) {
+    console.log("Comprador maior de idade")
+    listaDeDestinos.splice(1,1)
+}
 
